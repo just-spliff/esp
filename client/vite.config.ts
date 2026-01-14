ebf19113645aea3cfbe5893215ac88d05575a593
Maciej Gierałtowski <kolorowy517@gmail.com>
2026-01-14 02:07:04 +0100

    .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,6 +19,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "../server/public",
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       "/api": {
