8b8e4debd0c24c68aa9d85c3340266fd62e19acf
Maciej Gierałtowski <kolorowy517@gmail.com>
2026-01-14 02:11:11 +0100

    .

--- client/src/routes/control.tsx
+++ client/src/routes/control.tsx
@@ -38,7 +38,7 @@ export const Route = createFileRoute("/control")({
 });
 
 // ===================== CONFIG =====================
-const BROKER_URL = "wss://caddy-production-fba2.up.railway.app/mqtt";
+const BROKER_URL = "wss://mechanical-fey-spliff-ed1ace61.koyeb.app/mqtt";
 const DEVICE_ID = "3C893E124B00";
 const TOPIC_BASE = "fsrmag";
 
